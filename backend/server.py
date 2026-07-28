3b75f7f930994a8c4afe70f01c637d4bb72d1703
JaichandJohnson <123587610+JaichandJohnson@users.noreply.github.com>
2026-07-29 03:12:52 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -442,6 +442,7 @@ async def google_login(payload: GoogleLoginPayload, response: Response):
         "email": email,
         "name": name,
         "picture": picture,
+         "session_token": session_token,
     }
 
 
